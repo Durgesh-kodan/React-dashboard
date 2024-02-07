9f4f32bbca34e09159c8e861cdef7f28d9bac0cc
Durgesh <durgeshkodan101@gmail.com>
2024-02-08 02:40:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Layout from './components/Layout/Layout';
 import Calendar from './pages/Calendar/Calendar';
 import BoardPage from './pages/Board/Board';
+import DataGrid from './pages/DataGrid/DataGrid';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path='dashboard' element={<Dashboard/>}></Route>
                 <Route path='calendar' element={<Calendar/>}></Route>
                 <Route path='board' element={<BoardPage/>}></Route>
+                <Route path='users' element={<DataGrid/>}></Route>
             </Route>
         </Routes>
         </BrowserRouter>
